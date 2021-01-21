cc44735a64bfced41aa96142a7bd7998ef8cb3a0
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-21 21:13:21 +0200

    Add files via upload

---
+++ Task01_Furniture/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task01_Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>([A-Za-z]+)<<(\d+\.?\d*)!(\d+)";
+
+            Regex chek = new Regex(pattern);
+
+            double spendMoney = 0;
+            
+            string nextInputLine = Console.ReadLine();
+            
+            Console.WriteLine("Bought furniture:");
+
+            while (nextInputLine.ToUpper() != "PURCHASE")
+            {
+                
+                Match validDatas = chek.Match(nextInputLine);
+
+                if(validDatas.Success)
+                {
+                    string nextFurniture = validDatas.Groups[1].Value;
+                    double nextPrice = double.Parse(validDatas.Groups[2].Value);
+                    int nextQuantitty = int.Parse(validDatas.Groups[3].Value);
+
+                    spendMoney += nextPrice * nextQuantitty;
+
+                    Console.WriteLine(nextFurniture);
+
+                }
+
+                 nextInputLine = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total money spend: {spendMoney:f2}");
+        }
+    }
+}
