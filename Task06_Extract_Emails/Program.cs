7efec28d4406122811b5b89fa4238c18f54c1ae6
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-21 21:19:24 +0200

    Add files via upload

---
+++ Task06_Extract_Emails/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task06_Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"((?<=\s)[a-zA-Z0-9]+([-.]\w*)*@[a-zA-Z]+?([.-][a-zA-Z]*)*(\.[a-z]{2,}))";  // [A-z0-9-_\.]+@[A-z]+.[A-z]+\.[A-z]+
+
+            Regex emailExtract = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            MatchCollection allEmails = emailExtract.Matches(text);
+
+            foreach (Match item in allEmails)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
