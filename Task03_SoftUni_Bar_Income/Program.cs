2ecd0ab3f0bbb2219d22b41042a14cbe462bc3ad
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-21 21:16:32 +0200

    Add files via upload

---
+++ Task03_SoftUni_Bar_Income/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task03_SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"%([A-Z][a-z]+)%[^|$%.]*<(\w+)>[^|$%.]*\|(\d+)\|[^|$%.]*?([0-9]*\.?[0-9]+)\$";
+
+            Regex extractInfo = new Regex(pattern);
+
+            double totallMoney = 0;
+
+            while (true)
+            {
+                string nextLine = Console.ReadLine();
+
+                if(nextLine.ToUpper() == "END OF SHIFT")
+                {
+                    break;
+                }
+
+                Match validInfo = extractInfo.Match(nextLine);
+
+                if (validInfo.Success)
+                {
+                    string customer = validInfo.Groups[1].Value;
+                    string product = validInfo.Groups[2].Value;
+                    int quanttity = int.Parse(validInfo.Groups[3].Value);
+                    double price = double.Parse(validInfo.Groups[4].Value);
+
+                    totallMoney += quanttity * price;
+
+                    Console.WriteLine($"{customer}: {product} - {(quanttity * price):f2}");
+                }
+            }
+
+
+            Console.WriteLine($"Total income: {totallMoney:f2}");
+        }
+    }
+}
